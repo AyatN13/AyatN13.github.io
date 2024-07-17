85590657bdc134232b072da9bcef2d6933bb1926
Ayat Noor <ayat13noor@gmail.com>
2024-07-17 13:05:57 -0400

    text

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const nextBtn = document.getElementById('nextBtn');
     const imageIndexDisplay = document.getElementById('imageIndex');
     const descriptions = [
-        "Image: CN Tower, Toronto Ontario",
-        "Image: Amsterdam Castle Muiderslot",
-        "Image: Isabella Stewart Garden Museum, Boston, MA"
+        "CN Tower, Toronto Ontario",
+        "Amsterdam Castle Muiderslot",
+        "Isabella Stewart Garden Museum, Boston, MA"
     ];
     let currentImageIndex = 0;
     let interval;
 
     function showImage(index) {
         images.forEach((img, i) => {
-            img.classList.toggle('active', i === index);
+            img.style.display = i === index ? 'block' : 'none';
         });
-        imageIndexDisplay.textContent = `${index + 1}/${images.length} ${descriptions[index]}`;
+        imageIndexDisplay.textContent = descriptions[index];
     }
 
     function nextImage() {
